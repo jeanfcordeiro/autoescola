4732255b9c0d65dce90b0a09cf9f1f1cd9b60e18
jeanfcordeiro <jeanf12.jfc@gmail.com>
2025-08-21 16:35:07 -0300

    ajustes finais de fontes e espaçamentos

--- js/script.js
+++ js/script.js
@@ -15,3 +15,10 @@ setInterval(() => {
         }
         document.getElementById('radio' + cont).checked = true;
     }
+
+const toggle = document.querySelector(".header__toggle");
+const nav = document.querySelector(".header__nav");
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("show");
+});
