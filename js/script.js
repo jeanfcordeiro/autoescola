adfe9d6f5bfba4d353040b9a89687c16f11db63a
jeanfcordeiro <jeanf12.jfc@gmail.com>
2025-08-26 16:14:58 -0300

    refatoração do menu

--- js/script.js
+++ js/script.js
@@ -16,9 +16,7 @@ setInterval(() => {
         document.getElementById('radio' + cont).checked = true;
     }
 
-const toggle = document.querySelector(".header__toggle");
-const nav = document.querySelector(".header__nav");
-
-toggle.addEventListener("click", () => {
-  nav.classList.toggle("show");
-});
+function clickMenu() {
+    const menu = document.getElementById("header__menu");
+    menu.classList.toggle("open");
+}
